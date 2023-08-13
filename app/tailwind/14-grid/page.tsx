0d9eacc939b733f41c947d6a74529f6929066a58
Kihyeon Kwon <kihyeon.kwn@gmail.com>
2023-08-14 03:14:56 +0900

    grid

---
+++ app/tailwind/14-grid/page.tsx
@@ -0,0 +1,20 @@
+export default function Page() {
+  return (
+    <>
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-2">
+        <div className="md:col-span-2  md:row-span-2 p-10 border border-blue-500 bg-blue-100 flex items-center justify-center">
+          col-span-2 and row-span-2
+        </div>
+        <div className="p-10 border border-blue-500 bg-blue-100"></div>
+        <div className="p-10 border border-blue-500 bg-blue-100"></div>
+        <div className="p-10 border border-blue-500 bg-blue-100"></div>
+        <div className="p-10 border border-blue-500 bg-blue-100"></div>
+        <div className="p-10 border border-blue-500 bg-blue-100"></div>
+        <div className="p-10 border border-blue-500 bg-blue-100"></div>
+        <div className="p-10 border border-blue-500 bg-blue-100"></div>
+        <div className="p-10 border border-blue-500 bg-blue-100"></div>
+        <div className="p-10 border border-blue-500 bg-blue-100"></div>
+      </div>
+    </>
+  );
+}
