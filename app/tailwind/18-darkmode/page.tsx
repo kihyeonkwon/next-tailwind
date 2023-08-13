608186834a87319d00d63c03e2567de2c5aa8d93
Kihyeon Kwon <kihyeon.kwn@gmail.com>
2023-08-14 04:07:28 +0900

    dark mode

--- app/tailwind/18-darkmode/page.tsx
+++ app/tailwind/18-darkmode/page.tsx
@@ -1,3 +1,25 @@
-export default function Page() {
-  return <div>Hello</div>;
+//interface for item
+interface Item {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+export default async function Page() {
+  // get dummy data from jsonplaceholder using fetch
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos/");
+  const data: Item[] = await response.json();
+
+  return (
+    <div className="container mx-auto mt-10 grid grid-cols-1 md:grid-cols-2 gap-2">
+      {data.map((item: Item) => (
+        <div className=" bg-white dark:bg-gray-800 dark:text-white shadow-lg shadow-emerald-200 px-8 py-4 my-4">
+          <h1>{item.title}</h1>
+          <h2>{item.userId}</h2>
+          <p className="text-lg">{item.completed}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
