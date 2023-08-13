5e062ad5c28be2fb28988087bda4c6b7b2ef8b9e
Kihyeon Kwon <kihyeon.kwn@gmail.com>
2023-08-13 23:31:10 +0900

    filters

---
+++ app/tailwind/9-filters/page.tsx
@@ -0,0 +1,120 @@
+export default function Page() {
+  return (
+    <>
+      <h1>Filters</h1>
+      <div className="blur-sm">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt, unde
+        dolorum minima officiis blanditiis possimus recusandae rerum
+        consequatur, in et nisi sequi. Totam rem quo esse nihil sequi doloribus
+        expedita!
+      </div>
+
+      <div
+        className="h-64 bg-cover bg-center w-screen blur-xl"
+        style={{
+          backgroundImage:
+            "url(https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80)",
+        }}
+      ></div>
+
+      <div
+        className="h-64 bg-cover bg-center w-screen brightness-50"
+        style={{
+          backgroundImage:
+            "url(https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80)",
+        }}
+      ></div>
+      <div
+        className="h-64 bg-cover bg-center w-screen brightness-75"
+        style={{
+          backgroundImage:
+            "url(https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80)",
+        }}
+      ></div>
+      <div
+        className="h-64 bg-cover bg-center w-screen brightness-100"
+        style={{
+          backgroundImage:
+            "url(https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80)",
+        }}
+      ></div>
+      <div
+        className="h-64 bg-cover bg-center w-screen contrast-0"
+        style={{
+          backgroundImage:
+            "url(https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80)",
+        }}
+      ></div>
+      <div
+        className="h-64 bg-cover bg-center w-screen contrast-50"
+        style={{
+          backgroundImage:
+            "url(https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80)",
+        }}
+      ></div>
+      <div
+        className="h-64 bg-cover bg-center w-screen contrast-150"
+        style={{
+          backgroundImage:
+            "url(https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80)",
+        }}
+      ></div>
+      <div
+        className="h-64 bg-cover bg-center w-screen contrast-150"
+        style={{
+          backgroundImage:
+            "url(https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80)",
+        }}
+      ></div>
+      <div
+        className="h-64 bg-cover bg-center w-screen grayscale"
+        style={{
+          backgroundImage:
+            "url(https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80)",
+        }}
+      ></div>
+      <div
+        className="h-64 bg-cover bg-center w-screen invert"
+        style={{
+          backgroundImage:
+            "url(https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80)",
+        }}
+      ></div>
+      <div
+        className="h-64 bg-cover bg-center w-screen sepia"
+        style={{
+          backgroundImage:
+            "url(https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80)",
+        }}
+      ></div>
+      <div
+        className="h-64 bg-cover bg-center w-screen hue-rotate-15"
+        style={{
+          backgroundImage:
+            "url(https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80)",
+        }}
+      ></div>
+      <div
+        className="h-64 bg-cover bg-center w-screen hue-rotate-60"
+        style={{
+          backgroundImage:
+            "url(https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80)",
+        }}
+      ></div>
+      <div
+        className="h-64 bg-cover bg-center w-screen hue-rotate-90"
+        style={{
+          backgroundImage:
+            "url(https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80)",
+        }}
+      ></div>
+      <div
+        className="h-64 bg-cover bg-center w-screen hue-rotate-180"
+        style={{
+          backgroundImage:
+            "url(https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80)",
+        }}
+      ></div>
+    </>
+  );
+}
