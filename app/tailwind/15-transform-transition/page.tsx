9b384d614a45b82de37c02d29d8f1ebc3a5c14ad
Kihyeon Kwon <kihyeon.kwn@gmail.com>
2023-08-14 03:24:51 +0900

    transform and transition

---
+++ app/tailwind/15-transform-transition/page.tsx
@@ -0,0 +1,14 @@
+export default function Page() {
+  return (
+    <>
+      <div>
+        <button className="px-8 py-2 m-24 text-white bg-blue-500 rounded hover:bg-blue-700 transition duration-500">
+          Button
+        </button>
+        <button className="px-8 py-2 m-24 text-white bg-blue-500 rounded hover:bg-blue-700 hover:rotate-12 transition-transform">
+          Button
+        </button>
+      </div>
+    </>
+  );
+}
