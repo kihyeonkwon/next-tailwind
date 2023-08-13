e256365df00fa4893503e74c3870c2fdae471917
Kihyeon Kwon <kihyeon.kwn@gmail.com>
2023-08-13 15:25:09 +0900

    typography

---
+++ app/tailwind/4/page.tsx
@@ -0,0 +1,146 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>Fonts</h1>
+      <div className="font-sans">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam quasi
+        aperiam assumenda vero natus, dolorem quo, eius libero nulla officiis
+        nemo repellat veritatis a culpa unde sit, provident cupiditate quas.
+      </div>
+      <div className="font-serif">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis, iusto
+        molestias! Fugiat, fugit in. Excepturi tempore architecto animi ratione,
+        deleniti quo qui suscipit rerum sunt consectetur odio, recusandae amet
+        mollitia!
+      </div>
+      <div className="font-mono">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta maiores
+        ea impedit natus laboriosam, cumque nam obcaecati, necessitatibus
+        blanditiis, sapiente magnam tempora praesentium iste debitis! Quos nisi
+        voluptatum ullam minus.
+      </div>
+
+      <h1>Font Size</h1>
+      <div className="text-xs">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore vel
+        ducimus, et autem dolores a saepe eveniet consectetur iusto. Blanditiis
+        vero ea tenetur nesciunt consectetur quae, cupiditate iusto ullam?
+        Inventore.
+      </div>
+      <div className="text-sm">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore vel
+        ducimus, et autem dolores a saepe eveniet consectetur iusto. Blanditiis
+        vero ea tenetur nesciunt consectetur quae, cupiditate iusto ullam?
+        Inventore.
+      </div>
+      <div className="text-base">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore vel
+        ducimus, et autem dolores a saepe eveniet consectetur iusto. Blanditiis
+        vero ea tenetur nesciunt consectetur quae, cupiditate iusto ullam?
+        Inventore.
+      </div>
+      <div className="text-lg">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore vel
+        ducimus, et autem dolores a saepe eveniet consectetur iusto. Blanditiis
+        vero ea tenetur nesciunt consectetur quae, cupiditate iusto ullam?
+        Inventore.
+      </div>
+      <div className="text-xl">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore vel
+        ducimus, et autem dolores a saepe eveniet consectetur iusto. Blanditiis
+        vero ea tenetur nesciunt consectetur quae, cupiditate iusto ullam?
+        Inventore.
+      </div>
+      <div className="text-2xl">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore vel
+        ducimus, et autem dolores a saepe eveniet consectetur iusto. Blanditiis
+        vero ea tenetur nesciunt consectetur quae, cupiditate iusto ullam?
+        Inventore.
+      </div>
+      <div className="text-3xl">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore vel
+        ducimus, et autem dolores a saepe eveniet consectetur iusto. Blanditiis
+        vero ea tenetur nesciunt consectetur quae, cupiditate iusto ullam?
+        Inventore.
+      </div>
+
+      <h1>Font Weight</h1>
+      <div className="font-light">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id laudantium
+        itaque explicabo, numquam voluptatibus nam deleniti expedita debitis
+        deserunt minima architecto quis sapiente minus, quisquam nemo
+        necessitatibus, mollitia sequi suscipit.
+      </div>
+      <div className="font-normal">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id laudantium
+        itaque explicabo, numquam voluptatibus nam deleniti expedita debitis
+        deserunt minima architecto quis sapiente minus, quisquam nemo
+        necessitatibus, mollitia sequi suscipit.
+      </div>
+      <div className="font-medium">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id laudantium
+        itaque explicabo, numquam voluptatibus nam deleniti expedita debitis
+        deserunt minima architecto quis sapiente minus, quisquam nemo
+        necessitatibus, mollitia sequi suscipit.
+      </div>
+      <div className="font-semibold">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id laudantium
+        itaque explicabo, numquam voluptatibus nam deleniti expedita debitis
+        deserunt minima architecto quis sapiente minus, quisquam nemo
+        necessitatibus, mollitia sequi suscipit.
+      </div>
+      <div className="font-bold">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id laudantium
+        itaque explicabo, numquam voluptatibus nam deleniti expedita debitis
+        deserunt minima architecto quis sapiente minus, quisquam nemo
+        necessitatibus, mollitia sequi suscipit.
+      </div>
+
+      <h1>Letter Spacing </h1>
+      <div className="tracking-tight">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste ullam
+        libero autem, unde et provident ab suscipit voluptas qui quasi minus
+        doloremque, neque quae perferendis beatae est voluptates sequi
+        molestias.
+      </div>
+      <div className="tracking-normal">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste ullam
+        libero autem, unde et provident ab suscipit voluptas qui quasi minus
+        doloremque, neque quae perferendis beatae est voluptates sequi
+        molestias.
+      </div>
+      <div className="tracking-wide">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste ullam
+        libero autem, unde et provident ab suscipit voluptas qui quasi minus
+        doloremque, neque quae perferendis beatae est voluptates sequi
+        molestias.
+      </div>
+      <div className="text-left">text left</div>
+      <div className="text-center">text center</div>
+      <div className="text-right">text right</div>
+
+      <h1>Text Decoration</h1>
+      <div className="underline decoration-4 decoration-blue-400">
+        underline
+      </div>
+      <div className="underline decoration-4 decoration-blue-400 underline-offset-2">
+        underline
+      </div>
+      <div className="underline decoration-double decoration-blue-400">
+        underline
+      </div>
+      <div className="underline decoration-dotted decoration-blue-400">
+        underline
+      </div>
+      <div className="underline decoration-wavy decoration-blue-400">
+        underline
+      </div>
+
+      <h1>Text Transform</h1>
+      <div className="uppercase">uppercase</div>
+      <div className="lowercase">lowercase</div>
+      <div className="capitalize">capitalize</div>
+      <div className="normal-case">normal-case</div>
+    </div>
+  );
+}
